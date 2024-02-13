82bf3972e66a2bcc7821a575e8693f17ee1641e3
SteMat89 <stemat@GUPPY.zoo.lan>
2024-02-13 13:27:41 +0200

    Engaging home page

--- app/app.py
+++ app/app.py
@@ -1,11 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
+import requests
+import os
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', landing_image='https://via.placeholder.com/500')
+    url = 'https://api.nasa.gov/planetary/apod'
+
+    headers = {"Accept": "application/json"}
+
+    query = {
+        'api_key': os.environ["API_KEY"],
+    }
+
+    response = requests.request(
+        "GET",
+        url,
+        headers=headers,
+        params=query
+    )
+
+    res = response.json()
+
+    hdurl = res['hdurl']
+    title = res['title']
+    explanation = res['explanation']
+    
+    
+    return render_template('index.html', landing_image=hdurl, landing_title = title,  landing_explanation = explanation)
 
 @app.route('/mars')
 def mars():
